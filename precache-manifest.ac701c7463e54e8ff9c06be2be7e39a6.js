46af3c20897fc46a9783f47a9e7dd53083af70c7
lworkman <liam.workman@gmail.com>
2019-01-19 17:55:23 -0800

    Updates

---
+++ precache-manifest.ac701c7463e54e8ff9c06be2be7e39a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8ccee89d1b41fee2014",
+    "url": "/battle-snake-board-generator/static/js/runtime~main.f8ccee89.js"
+  },
+  {
+    "revision": "c1d83719200bffda1fa0",
+    "url": "/battle-snake-board-generator/static/js/main.c1d83719.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/battle-snake-board-generator/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "c1d83719200bffda1fa0",
+    "url": "/battle-snake-board-generator/static/css/main.5899760b.chunk.css"
+  },
+  {
+    "revision": "6b0ed1885db5fc38841374364e295853",
+    "url": "/battle-snake-board-generator/index.html"
+  }
+];
